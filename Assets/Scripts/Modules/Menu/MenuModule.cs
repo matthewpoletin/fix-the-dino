0b8d5f966dab80dd5cf5924cb6ed4803ed97b1e6
Matvey Poletin <poletinm@yandex.ru>
2020-02-02 17:18:56 +0300

    Случайная картинка в виджете меню

--- Assets/Scripts/Modules/Menu/MenuModule.cs
+++ Assets/Scripts/Modules/Menu/MenuModule.cs
@@ -1,17 +1,24 @@
-using System.Linq;
+using System.Collections.Generic;
 using Core;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace Modules.Menu
 {
     public class MenuModule : BaseModule, IPointerDownHandler
     {
+        [SerializeField] private Image _backgroundImage = null;
+        [SerializeField] private List<Sprite> _backgroundSprites = null;
+
         private GameController _gameController;
 
         public override void Activate(GameController gameController, Canvas canvas)
         {
             _gameController = gameController;
+
+            var sprite = _backgroundSprites[Random.Range(0, _backgroundSprites.Count)];
+            _backgroundImage.sprite = sprite;
         }
 
         public override void OnModuleLoaded()
